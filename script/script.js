3f378463beb978755a28700c80aa5209f912321f
LilMus <mustapha.seghir@gmail.com>
2022-05-31 04:35:23 +0200

    final version

--- script/script.js
+++ script/script.js
@@ -3,29 +3,101 @@ const amount = [];
 const chartSpendingsValue = document.getElementsByClassName(
 	"chart-spendings-value"
 );
-const tableau = [5, 32, 89];
+const chartDays = document.getElementsByClassName("chart-days");
+const bars = document.querySelectorAll(".chart-spendings div");
+let maxSpendingValue = 0;
+let maxSpendingDay = "";
+let maxSpendingIndex = 0;
 
-fetch("../data.json")
-	.then((res) => res.json())
-	.then((datas) => {
-		setData(datas);
-	})
-	.catch((error) => {
-		console.error(error);
-	});
-
-function setData(datas) {
-	for (data of datas) {
-		days.push(data.day);
-		amount.push(data.amount);
-	}
+async function getData() {
+	const res = await fetch("../data.json");
+	const data = await res.json();
+	setData(data);
 }
 
 function setAmount() {
-	for (let i = 0; i < 7; i++) {
-		console.log(days[i]);
-		// chartSpendingsValue[i].innerText
+	for (let i = 0; i < amount.length; i++) {
+		chartSpendingsValue[i].innerHTML = `$${amount[i]}`;
 	}
 }
 
-setAmount();
+function setDays() {
+	for (let i = 0; i < days.length; i++) {
+		chartDays[i].innerHTML = days[i];
+	}
+}
+
+function displayChartSpendingsValue(element) {
+	element.style.opacity = 1;
+}
+
+function hideChartSpendingsValue(element) {
+	element.style.opacity = 0;
+}
+
+function addMouseoverEvent() {
+	for (let i = 0; i < bars.length; i++) {
+		bars[i].addEventListener("mouseover", function () {
+			displayChartSpendingsValue(chartSpendingsValue[i]);
+			changeHoverColor(bars[i]);
+		});
+	}
+}
+
+function addMouseleaveEvent() {
+	for (let i = 0; i < bars.length; i++) {
+		bars[i].addEventListener("mouseleave", function () {
+			hideChartSpendingsValue(chartSpendingsValue[i]);
+			resetHoverColor(bars[i]);
+		});
+	}
+}
+
+function lookForMaxSpending() {
+	for (let i = 0; i < amount.length; i++) {
+		if (amount[i] > maxSpendingValue) {
+			maxSpendingValue = amount[i];
+			maxSpendingIndex = i;
+		}
+	}
+}
+
+function colorMaxSpendingBar() {
+	bars[maxSpendingIndex].style.backgroundColor = "hsl(186, 34%, 60%)";
+}
+
+function changeHoverColor(element) {
+	element.style.opacity = 0.7;
+}
+
+function resetHoverColor(element) {
+	element.style.opacity = 1;
+}
+
+function setBarsHeight() {
+	let ratio = 1;
+	let i = 0;
+	for (value of amount) {
+		ratio = value / maxSpendingValue;
+		barHeight = ratio * 180;
+		console.log(barHeight);
+		bars[i].style.height = `${barHeight}px`;
+		i++;
+	}
+}
+
+function setData(datas) {
+	for (let data of datas) {
+		days.push(data.day);
+		amount.push(data.amount);
+	}
+	setAmount();
+	setDays();
+	lookForMaxSpending();
+	colorMaxSpendingBar();
+	setBarsHeight();
+}
+
+getData();
+addMouseoverEvent();
+addMouseleaveEvent();
